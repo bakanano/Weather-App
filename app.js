c4ac69db7c2f531ef7f29129921453cb7a28a6c8
bakanano <17448534+bakanano@users.noreply.github.com>
2021-01-28 18:03:46 +1300

    Update app.js

--- app.js
+++ app.js
@@ -13,26 +13,37 @@ window.addEventListener("load", () => {
 
 });
 
+/**
+ * 
+ * This function uses a proxy to bypass DarkSky blocking access via localhost, fetches user's
+ * geolocation coordinates (passed in as arguments) and returns the data as a JSON object. After
+ * the data has been read via response.json(), it is passed to the parseData(data) function for manipulation
+ * of the DarkSky API.
+ * 
+ **/
 function processData(lat, long) {
     const proxy = "https://cors-anywhere.herokuapp.com/";
-    /**template literals need to be encolosed in backticks**/
     const api = `${proxy}https://api.darksky.net/forecast/${config.key}/${lat},${long}`;
 
     fetch(api)
         .then(response => {
-            return response.json();
+            response.json();
         })
         .then(data => {
-            console.log(data);
             parseData(data);
         });
 }
 
+/**
+ * 
+ * 
+ * This function fetches the required data to display the user's location and current weather at their location.
+ * 
+ **/
 function parseData(data) {
     const temperatureDegrees = document.querySelector("#temperatureInDegrees");
     const tempDescription = document.querySelector("#temperatureDescription");
     const usersRegion = document.querySelector("#userLocation");
-    //icon is the icon text
     const {icon, summary, temperature} = data.currently;
      tempDescription.textContent = summary;
      temperatureDegrees.textContent = Math.round((temperature - 32) * (5 / 9), 2);
@@ -40,6 +51,13 @@ function parseData(data) {
      
      setIcon(icon, document.querySelector("#icon"));
 }
+
+/**
+ * 
+ * 
+ * This function sets the icons to show on the page.
+ * 
+ **/
 
 function setIcon(iconText, iconID) {
     const skycons = new Skycons({color: "white"});
